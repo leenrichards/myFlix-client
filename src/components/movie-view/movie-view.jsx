fb464c8a757dac5c00ec4968da785700ef367734
leenrichards <leenrichards@yahoo.com>
2022-02-11 10:25:09 -0800

    Added login view

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -5,14 +5,31 @@ import './movie-view.scss';
 
 
 export class MovieView extends React.Component {
+
+    keypressCallback(event) {
+        console.log(event.key);
+    }
+
+    //Add Keypress event listener
+    componentDidMount() {
+        document.addEventListener('keypress', this.keypressCallback);
+    }
+
+    //Unmount event listener
+    componentWillUnmount() {
+        document.removeEventListener('keypress', this.keypressCallback);
+    }
+
+    //render movie view
     render() {
         const { movie, onBackClick } = this.props;
         return (
 
             <div className="movie-view" >
+
                 <div className="movie-poster" >
-                    <img src={movie.ImagePath} crossOrigin="anoymous" className='movie-image' />
-                </div>
+                    <img src={movie.ImagePath} crossOrigin="anonymous" className='movie-image' />
+                </div >
                 <div className="movie-title">
                     <span className="label">Title: </span>
                     <span className="value">{movie.Title}</span>
@@ -23,8 +40,10 @@ export class MovieView extends React.Component {
                 </div>
                 <div className="movie-genre">
                     <span className="label">Genre: </span>
-                    <span className="value">{movie.Genre}</span>
+                    <span className="value">{movie.Genre.Name}</span>
                 </div>
+
+
                 <button onClick={() => { onBackClick(null); }}>Back</button>
             </div>
         )
